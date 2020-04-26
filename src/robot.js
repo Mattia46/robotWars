fac84854fa4602c6c0fdde8ee32dbb06d9bd9cfb
Mattia46 <mattia.assogna@gmail.com>
2020-04-26 12:19:40 +0100

    added Robot object

---
+++ src/robot.js
@@ -0,0 +1,36 @@
+const moveRobot = ({ gameArea, position }) => {
+  if(position.heading === "N" && position.y < gameArea.max_y) position.y++;
+  if(position.heading === "S" && position.y > 0) position.y--;
+  if(position.heading === "E" && position.x < gameArea.max_x) position.x++;
+  if(position.heading === "W" && position.x > 0) position.x--;
+
+  return position;
+}
+
+const mappingAction = {
+  'N': action => action === 'L' ? 'W' : 'E',
+  'S': action => action === 'L' ? 'E' : 'W',
+  'W': action => action === 'L' ? 'S' : 'N',
+  'E': action => action === 'L' ? 'N' : 'S',
+}
+const rotateRobot = ({ position,  nextAction}) => {
+  position.heading = mappingAction[position.heading](nextAction);
+  return position;
+}
+
+const getPosition = ({ position }) => {
+  console.log('Current position is: ', position)
+  return position;
+}
+
+const initRobot = ({ position }) => ({
+  position,
+  move: ({ gameArea, nextAction }) => {
+    nextAction === 'M'
+      ? moveRobot({ gameArea, position})
+      : rotateRobot({ position, nextAction })
+  },
+  getCurrentPosition: () => getPosition({ position }),
+});
+
+module.exports = { initRobot };
--- tests/robot.test.js
+++ tests/robot.test.js
@@ -1,5 +1,54 @@
+const { initRobot } = require('../src/robot');
+
 describe('Robot: ', () => {
-  it('init test', () => {
-    expect(true).toBe(true);
+  const gameArea = { max_x: 5, max_y: 5};
+
+  describe('should Move: ', () => {
+    it('towards Y axis', () => {
+      const position = { x: 1, y: 3, heading: 'N' };
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'M' });
+      expect(robot.position).toEqual({ ...position, y: 4 });
+    });
+
+    it('towards X axis', () => {
+      const position = { x: 1, y: 3, heading: 'E' };
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'M' });
+      expect(robot.position).toEqual({ ...position, x: 2 });
+    });
+  });
+
+  describe('should Not Move: ', () => {
+
+    it('towards Y axis', () => {
+      const position = { x: 5, y: 5, heading: 'N' };
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'M' });
+      expect(robot.position).toEqual(position);
+    });
+
+    it('towards X axis', () => {
+      const position = { x: 5, y: 5, heading: 'E' };
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'M' });
+      expect(robot.position).toEqual(position);
+    });
+  });
+
+  describe('should Rotate: ', () => {
+    it('towards W when heading N and action L', () => {
+      const position = { x: 5, y: 5, heading: 'N'};
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'L' });
+      expect(robot.position).toEqual({...position, heading: 'W'});
+    });
+
+    it('towards E when heading N and action R', () => {
+      const position = { x: 5, y: 5, heading: 'N'};
+      const robot = initRobot({ position });
+      robot.move({ gameArea, nextAction: 'R' });
+      expect(robot.position).toEqual({...position, heading: 'E'});
+    });
   });
 });
